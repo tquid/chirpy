7e9e295dfb0b0993d3fb1642aa045281fca7ce9f
JB Bell <tquidca@gmail.com>
2024-08-22 11:15:07 -0700

    Restrict methods

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
