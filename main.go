3af7f2e21a5573e85dbf69e5e5e052bc0c2f3142
JB Bell <tquidca@gmail.com>
2024-10-08 15:07:03 -0700

    Revert to solution-supplied code and add DB goodies

--- main.go
+++ main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	"github.com/tquid/chirpy/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	fileserverHits int
+	fileserverHits atomic.Int32
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("unable to open connection to %s: %v", dbURL, err)
+	}
+	_ = database.New(db)
 	const filepathRoot = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
-		fileserverHits: 0,
+		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/chirps", handlerPostChirp)
-	mux.HandleFunc("GET /api/chirps", handlerGetChirps)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/api/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
--- readiness.go
+++ readiness.go
@@ -7,4 +7,3 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
-
